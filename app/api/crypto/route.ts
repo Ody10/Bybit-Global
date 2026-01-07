ef5bf3909230a3847b8440508fa23b8174ad10d2
Bold0077 <142579563+Bold0077@users.noreply.github.com>
2026-01-07 01:07:09 +0000

    Add dynamic and revalidate settings to route

--- app/api/crypto/route.ts
+++ app/api/crypto/route.ts
@@ -1,4 +1,7 @@
 // app/api/crypto/route.ts
+export const dynamic = 'force-static';
+export const revalidate = false;
+
 export async function GET() {
   const response = await fetch(
     'https://api.bybit.com/v5/market/tickers?category=spot'
